204f85623c531bae08273e065ee6d01fffcc1c67
galesy <galesyfailsy@gmail.com>
2025-11-02 16:22:50 -0300

    fixing walljump with the worst code

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -8,6 +8,7 @@ const JUMPFORCE = -450.0
 var moveweight: float = 0.0:
 	set(value):
 		moveweight = clampf(value, 0.0, 3.0)
+var direction_weight = 0.05
 
 #How long it takes to reach the base speed value
 const BASE_ACCELERATION_TIME = 1.0
@@ -31,6 +32,8 @@ var dash_buffer = 0.0
 const MAX_dashbuffer = 0.1
 var coyote_time = 0.0
 const MAX_coyote_time = 0.1
+const CRASH_VALUE: float = 0.1
+var crash: float = 0.0
 
 func input_buffers():
 	if jump_buffer > 0.0:
@@ -52,23 +55,16 @@ func move_process():
 	if !is_on_floor():
 		velocity += get_gravity() * get_physics_process_delta_time()
 	
-	if jump_buffer > 0.0 and coyote_time > 0:
-		coyote_time = 0.0
-		jump_buffer = 0.0
-		velocity.y = JUMPFORCE
-		if is_on_wall_only():
-			velocity.x = get_wall_normal().x * SPEED
-			target_direction = get_wall_normal().x
-	elif Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y /= 2
+	if direction_weight != 0.05:
+		direction_weight = lerpf(direction_weight, 0.05, 0.01)
 	
 	var input = Input.get_axis("left", "right")
-	var was_still = is_equal_approx(velocity.x, 0.0)
+	var was_still = velocity.is_equal_approx(Vector2.ZERO)
 	if input:
 		if was_still:
 			target_direction = input
 		else:
-			target_direction = lerpf(target_direction, input, 0.05)
+			target_direction = lerpf(target_direction, input, direction_weight)
 		if signf(target_direction) == signf(input):
 			if velocity.x < SPEED and velocity.x > -SPEED:
 				moveweight += get_physics_process_delta_time() / BASE_ACCELERATION_TIME
@@ -83,6 +79,16 @@ func move_process():
 	else:
 		velocity.x = lerpf(0.0, SPEED * target_direction, moveweight)
 	
+	if jump_buffer > 0.0 and coyote_time > 0:
+		coyote_time = 0.0
+		jump_buffer = 0.0
+		if is_on_wall():
+			velocity.x = get_wall_normal().x * JUMPFORCE
+			target_direction = get_wall_normal().x
+		velocity.y = JUMPFORCE
+		
+	elif Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y /= 2
 	
 	if dash_buffer > 0 and !dash_used:
 		dash_buffer = 0.0
@@ -93,8 +99,12 @@ func move_process():
 	move_and_slide()
 	
 	if is_on_wall():
-		moveweight = 0.0
-		
+		crash += get_physics_process_delta_time()
+		if crash >= CRASH_VALUE:
+			moveweight = 0.0
+	else:
+		crash = 0.0
+	
 
 
 #Combat handling
