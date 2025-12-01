ca1993cdd72ae8b0d4f1d4d67db393c1b3ae938b
galesy <galesyfailsy@gmail.com>
2025-12-01 14:12:58 -0300

    clean slate for easier working

--- characters/player/boombox/boombox_weapon.gd
+++
@@ -1,9 +0,0 @@
-extends Weapon
-
-const BOOMBOX_PROJECTILE = preload("uid://d356ji37gv1ou")
-
-func fire():
-	var projectile = BOOMBOX_PROJECTILE.instantiate()
-	get_tree().root.add_child(projectile)
-	projectile.rotation = get_local_mouse_position().angle()
-	projectile.position = global_position
