22fb3694d19aa42f2c7db29f4f44d204cab44115
galesy <galesyfailsy@gmail.com>
2025-12-06 11:05:56 -0300

    revert physgrapple attempt

--- characters/player/playerbody.gd
+++ characters/player/playerbody.gd
@@ -21,6 +21,7 @@ var string_recharge = 0.0
 const GRAPPLE_PULLSPEED = SPEED * 2.0
 var grappling = false
 var grappletarget = Vector2.ZERO
+var grappledist = 0.0
 var grapple_collider: PhysicsBody2D
 
 func _physics_process(delta: float) -> void:
@@ -46,39 +47,44 @@ func _physics_process(delta: float) -> void:
 	
 	grapple_ray.target_position = get_local_mouse_position().normalized() * 320
 	
-	if !grappling:
-		if !is_on_floor(): 
-			if is_on_wall_only() and velocity.y > 0.0:
-				velocity += get_gravity() * delta / 10
-			else:
-				velocity += get_gravity() * delta
-			coyotetime -= delta
-		else: 
-			coyotetime = MAX_coyotetime
-		
-		if input:
-			moveweight += delta * airfactor
-			last_input = lerpf(last_input, input, 0.1)
+	if !is_on_floor(): 
+		if is_on_wall_only() and velocity.y > 0.0:
+			velocity += get_gravity() * delta / 10
 		else:
-			moveweight -= delta * airfactor
-		
-		if Input.is_action_just_pressed("grapple") and string > 0:
-			string -= 1
-			if grapple_ray.is_colliding():
-				grappletarget = grapple_ray.get_collision_point()
-			last_input = signf(get_local_mouse_position().x)
-			if last_input == 0.0: last_input = 1.0
-			grappling = true
-		
-		velocity.x = lerpf(0.0, SPEED * last_input, moveweight)
+			velocity += get_gravity() * delta
+		coyotetime -= delta
+	else: 
+		coyotetime = MAX_coyotetime
+	
+	if input:
+		moveweight += delta * airfactor
+		last_input = lerpf(last_input, input, 0.1)
 	else:
+		moveweight -= delta * airfactor
+	
+	if Input.is_action_just_pressed("grapple") and string > 0:
+		string -= 1
+		if grapple_ray.is_colliding():
+			grappletarget = grapple_ray.get_collision_point()
+			grappledist = global_position.distance_to(grappletarget)
+		last_input = signf(get_local_mouse_position().x)
+		if last_input == 0.0: last_input = 1.0
+		grappling = true
+	
+	velocity.x = lerpf(0.0, SPEED * last_input, moveweight)
+	if grappling:
 		if global_position.distance_squared_to(grappletarget) < 16**2 or global_position.distance_squared_to(grappletarget) > 400**2:
 			grappling = false
 		else:
-			if velocity.dot(global_position.direction_to(grappletarget)) < 0 and velocity.length() > SPEED / 2:
-				velocity = velocity.slerp(global_position.direction_to(grappletarget) * GRAPPLE_PULLSPEED, delta)
-			else:
-				velocity = global_position.direction_to(grappletarget) * GRAPPLE_PULLSPEED
+			var force = Vector2.ZERO
+			if grappledist > 16:
+				var grapple_force = global_position.direction_to(grappletarget) / 5 * grappledist 
+				var veldot = velocity.dot(grapple_force)
+				var damping = global_position.direction_to(grappletarget) * veldot * -2.0
+				force += grapple_force + damping
+			
+			velocity += force * delta
+			
 	if jumpbuffer > 0 and (coyotetime > 0 or grappling):
 		jumpbuffer = 0.0
 		coyotetime = 0.0
